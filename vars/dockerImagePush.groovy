f46df67c5d19d4b82e30f9a78d6c064a751af65f
nafeyah <nafeyahthenx@gmail.com>
2024-08-02 23:54:49 +0530

    docker build clean up

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -10,12 +10,19 @@
 //}
 
 
-def call(String aws_account_id, String region, String ecr_repoName){
+def call(String aws_account_id, String region, String ecr_repoName) {
+    withEnv(["AWS_ACCESS_KEY_ID=${env.AWS_ACCESS_KEY_ID}", "AWS_SECRET_ACCESS_KEY=${env.AWS_SECRET_ACCESS_KEY}"]) {
+        sh """
+            aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+            aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+            aws configure set region ${region}
 
-    sh """
-     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-    """
+            aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+            docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
 
-
+            aws configure set aws_access_key_id none
+            aws configure set aws_secret_access_key none
+            aws configure set region none
+        """
+    }
 }
